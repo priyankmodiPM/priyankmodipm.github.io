3e8850c1904b4f8855ca576be0efd23b7c571105
Priyank Modi <pmodi@adobe.com>
2023-08-31 18:25:28 +0530

    set dark theme on document ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,7 +145,7 @@ function scrollActive(){
             }
         })
     }catch{
-        
+
     }
 }
 window.addEventListener('scroll', scrollActive)
@@ -181,9 +181,14 @@ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-mo
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+document.onreadystatechange = () => {
+    document.body.classList['add'](darkTheme)
+    themeButton.classList['add'](iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
